17903122ea6f9aa4b1a4e2b1775d95865332f2da
lian1925 <lian1925@qq.com>
2017-10-11 11:02:34 +0800

    Site updated: 2017-10-11 11:02:33

---
+++ js/post.js
@@ -0,0 +1,10 @@
+// test jquery
+// $('.page p img').css('border', '5px solid #000');
+
+// first wrap
+// $('.page p img').wrap('<a data-fancybox="gallery" href="' + $(this).attr('src') + '")></a>')
+$('.page p img').each(function() {
+  var src = $(this).attr('src');
+  var content = '<a data-fancybox="gallery" href="' + src + '")></a>';
+  $(this).wrap('' + content + ')></a>')
+})
